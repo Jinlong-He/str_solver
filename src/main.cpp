c4664ef53d1b6d1ae92daf5b1ef8cc1ce0c7aadf
何锦龙 <hejl@hejinlongs-MacBook-Pro.local>
2019-12-16 16:59:27 +0800

    in

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "str_solver.hpp"
 
 int main(int argc, char* argv[]) {
+    cout << "I'm in..." << endl;
     StrSolver solver((string(argv[1])));
     solver.solve(string(argv[2]));
     return 0;
